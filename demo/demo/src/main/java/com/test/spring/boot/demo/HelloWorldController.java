33932f5116c0d6ac7cfbdb746118affc5ce1388d
kayteeila <60275231+kayteeila@users.noreply.github.com>
2020-03-16 16:50:48 +0000

    inital spring boot demo project

---
+++ demo/demo/src/main/java/com/test/spring/boot/demo/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.test.spring.boot.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
